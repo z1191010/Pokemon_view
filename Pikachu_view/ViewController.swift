207f2e93bd7e911b113ad599a8ff0c5c152c6b85
Kai.W <z1191010@gmail.com>
2022-11-16 12:29:11 +0800

    Initial Commit

---
+++ Pikachu_view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pikachu_view
+//
+//  Created by Kai on 2022/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
